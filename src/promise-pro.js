1e1c8b7bfacb71bcc5ca582eeeae2d8a853bea66
scarletsky <scarletsky1025@gmail.com>
2023-11-25 22:39:33 +0800

    refactor: use wrap instead of inherits

--- src/promise-pro.js
+++ src/promise-pro.js
@@ -4,49 +4,77 @@ export const STATUS_PENDING = 0;
 export const STATUS_FULFILLED = 1;
 export const STATUS_REJECTED = 2;
 
-export class PromisePro extends Promise {
+export class PromisePro {
   constructor(executor, abortController = new AbortController()) {
-    super((resolve, reject) => {
-      abortController.signal.addEventListener(EVENT_ABORT, reject);
-      return executor(resolve, reject, abortController.signal);
-    });
+    this.isPromisePro = true;
     this.status = STATUS_PENDING;
-    this.timeoutFuncId = null;
     this.abortController = abortController;
+    this.timeoutFuncId = null;
+    this.promise = new Promise((resolve, reject) => {
+      this.resolveFunc = (v) => {
+        this.status = STATUS_FULFILLED;
+        return resolve(v);
+      };
+
+      this.rejectFunc = (v) => {
+        this.status = STATUS_REJECTED;
+        return reject(v);
+      };
+
+      this.abortFunc = (v) => {
+        abortController.signal.removeEventListener(EVENT_ABORT, this.abortFunc);
+        return this.rejectFunc(v);
+      }
+
+      abortController.signal.addEventListener(EVENT_ABORT, this.abortFunc);
+      return executor(this.resolveFunc, this.rejectFunc, abortController.signal);
+    });
+  }
+
+  get isPending() {
+    return this.status === 0;
   }
 
   then(onFulfilled, onRejected) {
-    return super.then(
-      value => {
-        this.status = STATUS_FULFILLED;
-        if (onFulfilled) return onFulfilled(value);
-      },
-      reason => {
-        this.status = STATUS_REJECTED;
-        if (onRejected) return onRejected(reason);
-      }
-    );
+    return this.promise.then(onFulfilled, onRejected);
   }
 
   catch(onRejected) {
-    return super.catch(reason => {
-      this.status = STATUS_REJECTED;
-      if (onRejected) return onRejected(reason);
-    })
+    return this.promise.catch(onRejected);
   }
 
   abort(reason) {
+    if (!this.isPending) return this;
+
     if (!this.abortController.signal.aborted) {
       this.abortController.abort(reason);
     }
+
+    return this;
   }
 
   timeout(ms) {
+    if (!this.isPending) return this;;
+
+    this._clearTimeout();
+    this.timeoutFuncId = setTimeout(() => this.abort(EVENT_TIMEOUT), ms);
+
+    return this;
+  }
+
+  clean() {
+    this._clearAbort();
+    this._clearTimeout();
+  }
+
+  _clearAbort() {
+
+  }
+
+  _clearTimeout() {
     if (this.timeoutFuncId) {
       clearTimeout(this.timeoutFuncId);
       this.timeoutFuncId = null;
     }
-
-    this.timeoutFuncId = setTimeout(() => this.abort(EVENT_TIMEOUT), ms);
   }
 }
