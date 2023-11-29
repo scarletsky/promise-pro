607142352642e9870b584a0178361315b8a6b350
scarletsky <scarletsky1025@gmail.com>
2023-11-29 18:48:21 +0800

    test: update test

--- test/promise-pro.js
+++ test/promise-pro.js
@@ -84,3 +84,91 @@ test('PromisePro#timeout', async t => {
     }
   });
 });
+
+test('PromisePro.all', async t => {
+
+  t.test('`PromisePro` resolves', async t => {
+    const v = await PromisePro.all([
+      PromisePro.resolve(1),
+      PromisePro.resolve(2),
+    ]);
+    t.same(v, [1, 2]);
+  });
+
+  t.test('`PromisePro` rejects', async t => {
+    try {
+      await PromisePro.all([
+        PromisePro.reject(3),
+        PromisePro.resolve(4),
+      ]);
+    } catch (e) {
+      t.same(e, 3);
+    }
+  });
+
+  t.test('native `Promise` and `PromisePro` resolves', async t => {
+    const v = await PromisePro.all([
+      Promise.resolve(5),
+      PromisePro.resolve(6),
+    ]);
+    t.same(v, [5, 6]);
+  });
+
+  t.test('native `Promise` with `PromisePro` rejects', async t => {
+    try {
+      await PromisePro.all([
+        PromisePro.reject(7),
+        PromisePro.resolve(8),
+      ]);
+    } catch (e) {
+      t.same(e, 7);
+    }
+  });
+
+  t.test('abort', async t => {
+    const abortController = new AbortController();
+    const p1 = new PromisePro((resolve) => setTimeout(() => resolve(1), 1000), { abortController });
+    const p2 = new PromisePro((resolve) => setTimeout(() => resolve(2), 1000), { abortController });
+    const p = PromisePro.all([p1, p2], { abortController });
+    p.abort();
+
+    try {
+      await p;
+    } catch (e) {
+      t.equal(p1.status, STATUS_REJECTED);
+      t.equal(p2.status, STATUS_REJECTED);
+      t.equal(p.status, STATUS_REJECTED);
+    }
+  });
+});
+
+test('PromisePro#allSettled', async t => {
+
+  t.test('`PromisePro` resolves and rejects', async t => {
+    const v = await PromisePro.allSettled([
+      PromisePro.resolve(1),
+      new PromisePro((resolve, _) => setTimeout(() => resolve(2), 500)),
+      new PromisePro((_, reject) => setTimeout(() => reject(3), 200)),
+    ]);
+    t.equal(v[0].value, 1);
+    t.equal(v[1].value, 2);
+    t.equal(v[2].reason, 3);
+  });
+
+  t.test('abort', async t => {
+    const abortController = new AbortController();
+    const p1 = new PromisePro((resolve) => setTimeout(() => resolve(1), 1000), { abortController });
+    const p2 = new PromisePro((resolve) => setTimeout(() => resolve(2), 1000), { abortController });
+    const p = PromisePro.allSettled([p1, p2], { abortController });
+    p.abort();
+
+    try {
+      await p;
+    } catch (e) {
+      t.equal(p1.status, STATUS_REJECTED);
+      t.equal(p2.status, STATUS_REJECTED);
+      t.equal(p.status, STATUS_REJECTED);
+    }
+  });
+
+});
